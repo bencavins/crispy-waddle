48a75b36a67ef98b332547c4405227ed89804a11
Ben Cavins <bencavins@gmail.com>
2022-08-24 13:02:38 -0400

    add prediction

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from twitoff.models import DB, User, Tweet
 import os
+from twitoff.predict import predict_user
 
 from twitoff.twitter import add_or_update_user
 
@@ -23,6 +24,46 @@ def create_app():
     @app.route('/test')
     def test():
         return '<p>This  is a test</p>'
+
+    @app.route('/user', methods=['POST'])
+    def add_user():
+        username = request.values['user_name']
+        add_or_update_user(username)
+        user = User.query.filter(User.username == username).one()
+        return render_template(
+            'user.html', 
+            title=username, 
+            message='User added successfully!',
+            tweets=user.tweets,
+        )
+    
+    @app.route('/user/<name>')
+    def user(name=None):
+        user = User.query.filter(User.username == name).one()
+        return render_template(
+            'user.html', 
+            title=name, 
+            message='User added successfully!',
+            tweets=user.tweets,
+        )
+    
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        username0 = request.values['user0']
+        username1 = request.values['user1']
+        hypo_tweet_text = request.values['tweet_text']
+
+        if username0 == username1:
+            message = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(username0, username1, hypo_tweet_text)
+            if prediction:
+                predicted_user = username1
+            else:
+                predicted_user = username0
+            message = f'This tweet is more likely written by {predicted_user}'
+
+        return render_template('prediction.html', title='Prediction', message=message)
     
     @app.route('/reset')
     def reset():
